eeb531156d3aa3393b3622a1892e2709247b75d4
Stiven Castillo <stivencastillo.90@gmail.com>
2017-05-05 17:22:15 -0500

    AUTH: change estructure user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,22 +1,22 @@
 export class User {
 
 	public id: number;
-	public name: string;
+	public first_name: string;
+	public last_name: string;
 	public email: string;
 	public password: string;
 	public confirm_password: string;
-	public gender: string;
 	public remember_token: string;
 	public token: string;
 
-	constructor(id, name, email, password, confirm_password, gender, remember_token, token) {
-		
+	constructor(id, first_name, last_name, email, password, confirm_password, remember_token, token) {
+
 		this.id = id;
-		this.name = name;
+		this.first_name = first_name;
+		this.last_name = last_name;
 		this.email = email;
 		this.password = password;
 		this.confirm_password = confirm_password;
-		this.gender = gender;
 		this.remember_token = remember_token;
 		this.token = token;
 	}
